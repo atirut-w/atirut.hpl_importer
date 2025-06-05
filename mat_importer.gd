ccb2e21493274fd2f2ef47717b9c4c51653d3481
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-06-05 17:32:12 +0700

    Implement HPL3 material importer

--- mat_importer.gd
+++ mat_importer.gd
@@ -58,7 +58,47 @@ func _import(source_file: String, save_path: String, options: Dictionary, platfo
 		return ERR_CANT_OPEN
 	
 	var material := ShaderMaterial.new()
-
+	
+	# Load the HPL3 shader
+	var shader := load("res://addons/atirut.hpl_importer/hpl3.gdshader")
+	if shader == null:
+		push_error("Failed to load HPL3 shader")
+		return ERR_CANT_OPEN
+	material.shader = shader
+	
+	# Parse XML and extract texture file paths
+	var diffuse_path := ""
+	var normal_path := ""
+	var specular_path := ""
+	
+	while true:
+		var err := parser.read()
+		if err != OK:
+			break
+		
+		if parser.get_node_type() == XMLParser.NODE_ELEMENT:
+			var name := parser.get_node_name()
+			if name == "Diffuse":
+				diffuse_path = parser.get_named_attribute_value("File")
+			elif name == "NMap":
+				normal_path = parser.get_named_attribute_value("File")
+			elif name == "Specular":
+				specular_path = parser.get_named_attribute_value("File")
+	
+	# Load textures and assign to material uniforms
+	if diffuse_path != "":
+		var diffuse_tex = load(diffuse_path)
+		if diffuse_tex != null and diffuse_tex is Texture:
+			material.set_shader_parameter("diffuse_texture", diffuse_tex)
+	if normal_path != "":
+		var normal_tex = load(normal_path)
+		if normal_tex != null and normal_tex is Texture:
+			material.set_shader_parameter("normal_texture", normal_tex)
+	if specular_path != "":
+		var specular_tex = load(specular_path)
+		if specular_tex != null and specular_tex is Texture:
+			material.set_shader_parameter("specular_texture", specular_tex)
+	
 	var err := ResourceSaver.save(material, "%s.%s" % [save_path, _get_save_extension()])
 	if err != OK:
 		push_error("Failed to save material: %s" % err)
